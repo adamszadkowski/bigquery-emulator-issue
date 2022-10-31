674852016cab304174be06acb355c8b161f79fd7
Adam Szadkowski <adam.szadkowski@allegro.pl>
2022-10-31 12:52:56 +0100

    Test creation of dataset and table

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
+    implementation("com.google.cloud:google-cloud-bigquery:2.1.9")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("io.strikt:strikt-core:0.34.1")
 
--- src/test/kotlin/info/szadkowski/bqissue/SampleTest.kt
+++ src/test/kotlin/info/szadkowski/bqissue/SampleTest.kt
@@ -1,13 +1,40 @@
 package info.szadkowski.bqissue
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.bigquery.*
 import org.junit.jupiter.api.Test
-import strikt.api.expectThat
-import strikt.assertions.isTrue
 
 class SampleTest {
+    private val projectId = "<REPLACE_WITH_YOUR_PROJECT_ID>"
+    private val datasetId = "testingbq"
+    private val tableId = "mytablename"
 
     @Test
-    fun `do nothing`() {
-        expectThat(true).isTrue()
+    fun `create dataset and table`() {
+        val transportOptions = BigQueryOptions.getDefaultHttpTransportOptions()
+            .toBuilder()
+            .setConnectTimeout(6000)
+            .setReadTimeout(6000)
+            .build()
+
+        val service = BigQueryOptions.newBuilder()
+            .setTransportOptions(transportOptions)
+            .setLocation("EU")
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .build()
+            .service
+
+        service.create(
+            DatasetInfo.newBuilder(projectId, datasetId)
+                .setLocation("EU")
+                .build()
+        )
+
+        service.create(
+            TableInfo.of(
+                TableId.of(projectId, datasetId, tableId),
+                StandardTableDefinition.of(Schema.of(Field.of("id", StandardSQLTypeName.STRING)))
+            )
+        )
     }
 }
