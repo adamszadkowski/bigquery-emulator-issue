b55f1ffaa7e65f639d5f679c74ec65f99e853640
Adam Szadkowski <adam.szadkowski@allegro.pl>
2022-12-02 16:37:33 +0100

    Write and read some data

--- src/test/kotlin/info/szadkowski/bqissue/SampleTest.kt
+++ src/test/kotlin/info/szadkowski/bqissue/SampleTest.kt
@@ -1,10 +1,14 @@
 package info.szadkowski.bqissue
 
-import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.NoCredentials
 import com.google.cloud.bigquery.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
+import strikt.assertions.isFalse
 
 class SampleTest {
     private val host = "http://localhost:9050"
@@ -15,7 +19,7 @@ class SampleTest {
     private lateinit var service: BigQuery
 
     @BeforeEach
-    fun `configure bigquery`() {
+    fun `create dataset and table`() {
         val transportOptions = BigQueryOptions.getDefaultHttpTransportOptions()
             .toBuilder()
             .setConnectTimeout(6000)
@@ -26,9 +30,18 @@ class SampleTest {
             .setHost(host)
             .setTransportOptions(transportOptions)
             .setLocation("EU")
-            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .setProjectId(projectId)
+            .setCredentials(NoCredentials.getInstance())
             .build()
             .service
+
+        service.create(DatasetInfo.newBuilder(projectId, datasetId).setLocation("EU").build())
+
+        val schema = Schema.of(
+            Field.of("id", StandardSQLTypeName.STRING),
+            Field.of("otherProp", StandardSQLTypeName.STRING),
+        )
+        service.create(TableInfo.of(TableId.of(projectId, datasetId, tableId), StandardTableDefinition.of(schema)))
     }
 
     @AfterEach
@@ -36,20 +49,33 @@ class SampleTest {
         service.delete(DatasetId.of(projectId, datasetId), BigQuery.DatasetDeleteOption.deleteContents())
     }
 
-    @Test
-    fun `create dataset and table`() {
-        service.create(
-            DatasetInfo.newBuilder(projectId, datasetId)
-                .setLocation("EU")
-                .build()
-        )
+    @Nested
+    inner class `rest api saved data` {
 
-        val schema = Schema.of(Field.of("id", StandardSQLTypeName.STRING))
-        service.create(
-            TableInfo.of(
-                TableId.of(projectId, datasetId, tableId),
-                StandardTableDefinition.of(schema)
+        @BeforeEach
+        fun `store data`() {
+            val response = service.insertAll(
+                InsertAllRequest.newBuilder(TableId.of(datasetId, tableId))
+                    .addRow(mapOf("id" to "15432", "otherProp" to "some value here"))
+                    .build()
             )
-        )
+
+            expectThat(response.hasErrors()).isFalse()
+        }
+
+        @Test
+        fun `read data using rest api`() {
+            val query = "SELECT * FROM `$projectId.$datasetId.$tableId`"
+            val loaded = service.query(QueryJobConfiguration.of(query))
+                .values
+                .map { Result(it.get("id").stringValue, it.get("otherProp").stringValue) }
+
+            expectThat(loaded).containsExactly(Result("15432", "some value here"))
+        }
     }
 }
+
+data class Result(
+    val id: String,
+    val otherProp: String,
+)
