4ac998df1cfbfaaff5be09d068ed66fe0117de57
Adam Szadkowski <adam.szadkowski@allegro.pl>
2022-12-08 15:59:49 +0100

    Add spark to project

--- spark-usage/build.gradle.kts
+++ spark-usage/build.gradle.kts
@@ -4,7 +4,14 @@ plugins {
 }
 
 dependencies {
+    implementation("org.apache.spark:spark-core_2.12:2.4.8")
+    implementation("org.apache.spark:spark-sql_2.12:2.4.8")
+
     testImplementation("org.scalatest:scalatest_2.12:3.2.14")
     testImplementation("com.vladsch.flexmark:flexmark-all:0.64.0")
     testImplementation("org.pegdown:pegdown:1.6.0")
+}
+
+tasks.withType<ScalaCompile> {
+    targetCompatibility = ""
 }
